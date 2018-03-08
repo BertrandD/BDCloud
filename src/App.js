ce8c538ddf85706df21440b13c1d12db476489b0
Bertrand Darbon <bertrand.darbon@gmail.com>
2018-03-08 23:46:28 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import config from "./config.js";
 import {Route} from "react-router-dom";
 import './App.css';
-import { Photo, Video } from "react-gallery";
+import {Photo, Video} from "react-gallery";
 import Home from "./components/home/Home";
 import path from 'path';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Route exact path="/" component={Home}/>
-          <Route path="/" render={(props) => {
-              const p = window.location.pathname;
-              switch (path.extname(p)) {
-                  case '.mp4':
-                      return (
-                          <Video src={config.staticUrl + p}/>
-                      );
-                  case '.jpg':
-                  case '.jpeg':
-                  case '.JPG':
-                      return (
-                          <Photo src={config.staticUrl + p}/>
-                      );
-                  case '.mp3':
-                  case '.wav':
-                  case '.flac':
-                      return (
-                          <audio controls src={config.staticUrl + p}>Your browser does not support HTML5 audio. Sorry.</audio>
-                      );
-                  default:
-                      return(
-                          <div>
-                              File type not yet supported. Sorry.
-                          </div>
-                      )
-              }
-          }}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Route exact path="/" component={Home}/>
+                <Route path="/" render={(props) => {
+                    const p = window.location.pathname;
+                    switch (path.extname(p)) {
+                        case '.mp4':
+                            return (
+                                <Video src={config.staticUrl + p}/>
+                            );
+                        case '.jpg':
+                        case '.jpeg':
+                        case '.JPG':
+                            return (
+                                <Photo src={config.staticUrl + p}/>
+                            );
+                        case '.mp3':
+                        case '.wav':
+                        case '.flac':
+                            return (
+                                <audio controls src={config.staticUrl + p}>Your browser does not support HTML5 audio.
+                                    Sorry.</audio>
+                            );
+                        default:
+                            return (
+                                <div>
+                                    File type not yet supported. Sorry.
+                                </div>
+                            )
+                    }
+                }}/>
+                <footer>
+                    <span>
+                        Website made with &#10084; by Bertrand Darbon
+                    </span>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
